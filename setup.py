129eacd872cddf86596315f5a67c0185cbcfe27a
grra8002 <radoslaw.gregorczyk@nielseniq.com>
2023-01-21 23:17:37 +0100

    released on PyPi

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='HealthCheck_Pinger',
-    version='0.0.3',
-    author='Radosław Gregorczyk',
-    author_email='grerad@gmail.com',
-    description='easily ping HealthCheck server',
+    name="HealthCheck_Pinger",
+    version="0.0.4",
+    author="Gregorek85",
+    author_email="grerad@gmail.com",
+    description="easily ping HealthCheck server",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/Gregorek85/HealthCheck_Pinger',
-    project_urls = {
+    url="https://github.com/Gregorek85/HealthCheck_Pinger",
+    project_urls={
         "Bug Tracker": "https://github.com/Gregorek85/HealthCheck_Pinger/issues"
     },
-    license='MIT',
-    packages=['HealthCheck_Pinger'],
+    license="MIT",
+    packages=["HealthCheck_Pinger"],
 )
